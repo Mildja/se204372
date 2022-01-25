3e87384a97d57f18ec840d6c0ff089f69b65185c
artiyacham <athitiya.cham@ku.th>
2022-01-25 20:43:58 +0700

    Kate changes law naaaa

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,8 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("น.ส.วีรยา ศรีรุจิเมธากร 6220504780"); //PUT YOUR NAME HERE
+        groupMembers.add("น.ส.วีรยา ศรีรุจิเมธาก 6220504780"); //PUT YOUR NAME HERE
+        groupMembers.add("น.ส.อธิติยา จำเหล่ 6220502191"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
