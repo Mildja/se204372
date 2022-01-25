962acf4c0dd37691dc989383c189140f3b616058
njpor <nnppkk.j@gmail.com>
2022-01-25 20:56:57 +0700

    gooooooooooooo

--- OurGroup.java
+++ OurGroup.java
@@ -8,6 +8,7 @@ public class OurGroup {
         groupMembers = new Vector<String>();
         groupMembers.add("น.ส.วีรยา ศรีรุจิเมธาก 6220504780"); //PUT YOUR NAME HERE
         groupMembers.add("น.ส.อธิติยา จำเหล่ 6220502191"); //PUT YOUR NAME HERE
+        groupMembers.add("น.ส.นภัส เจียมประเสริฐ 6220502132");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
