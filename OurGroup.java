bc8d962eda2749ea9e479cf8e5aafed831ad182c
Namphueng25 <namphueng.k@ku.th>
2022-01-25 21:33:23 +0700

    Goodday Identity V

--- OurGroup.java
+++ OurGroup.java
@@ -10,6 +10,7 @@ public class OurGroup {
         groupMembers.add("น.ส.อธิติยา จำเหล่ 6220502191"); //PUT YOUR NAME HERE
         groupMembers.add("น.ส.นภัส เจียมประเสริฐ 6220502132");
         groupMembers.add("น.ส.รสริน เณรแตง 6220504763");
+        groupMembers.add("น.ส.น้ำผึ้ง กิติกังสดาร 6220502159");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
